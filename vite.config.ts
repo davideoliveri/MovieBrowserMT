8e0f879cfe2e63fc80ce058cd4a5815f0a814719
davideoliveri <davide.oliveri@gmail.com>
2025-07-24 13:26:15 +0200

    cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(() => {
   const isSsrClientBuild = process.env.VITE_BUILD_MODE === 'ssr_client';
@@ -11,14 +7,6 @@ export default defineConfig(() => {
   const ssrClientBuildOptions = {
     outDir: 'dist/client',
     ssrManifest: true,
-    rollupOptions: {
-      // This tells Vite to process index.html and use entry-client.tsx as its script.
-      // It correctly generates both the JS and the final index.html.
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        'entry-client': path.resolve(__dirname, 'src/entry-client.tsx'),
-      },
-    },
   };
 
   return {
