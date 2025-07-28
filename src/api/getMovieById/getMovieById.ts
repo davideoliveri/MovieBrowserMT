c865602cc4c41b36b25457808ee02f3ba4baeb85
davideoliveri <davide.oliveri@gmail.com>
2025-07-28 12:14:49 +0200

    improving the API

--- src/api/getMovieById/getMovieById.ts
+++ src/api/getMovieById/getMovieById.ts
@@ -1,12 +1,6 @@
 import { MovieDetailsData } from '@/interfaces/MovieDetailsDataInterface';
+import { apiClient } from '../apiClient';
 
-export async function getMovieById(id: string): Promise<MovieDetailsData> {
-  const apiKey = import.meta.env.VITE_TMDB_API_KEY;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=credits`
-  );
-  if (!res.ok) {
-    throw new Error(`Failed to fetch movie (${res.status})`);
-  }
-  return res.json() as Promise<MovieDetailsData>;
+export function getMovieById(id: string): Promise<MovieDetailsData> {
+  return apiClient(`/movie/${id}?append_to_response=credits`);
 }
